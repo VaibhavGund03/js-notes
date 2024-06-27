1a5fef4391e6904c4a22a726d8a4c6cba4a850c4
vaibhav gund <vaibhav.gcluster@gmail.com>
2024-06-27 07:57:40 +0530

    linking js to html

--- hello.js
+++ hello.js
@@ -1,4 +1,15 @@
-console.log("hello world")
+// js is programming language we use it to give instruction to the computer
 
-console.log("1+2")
-console.log("vaibhav")
+// vs code is a free and populer code editor by microsoft
+
+
+
+
+
+// console.log ("");  are use to print anything message 
+
+console.log("hello world");
+
+console.log("1+2");
+
+console.log("vaibhav");
---
+++ linking html.js
@@ -0,0 +1,5 @@
+// html is a alredy link with browser but js is not link with browser so process to js link with html.
+
+alert(error);
+
+// in html before </body> use  <script src"name of js file"></script>
