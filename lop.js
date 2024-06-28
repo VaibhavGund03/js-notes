ed56b2e0f9441c52920b87aa272329767930cca2
vaibhav gund <vaibhav.gcluster@gmail.com>
2024-06-28 14:39:46 +0530

    updated

---
+++ lop.js
@@ -0,0 +1,26 @@
+// loops are use to execute a piece of code again and again
+
+/* ======== for loop =========
+
+
+for(let i=1;i<=5;i++){
+console.log("javascript")
+}
+
+*/
+
+for(let i =1; i<=5;i++){
+    console.log("vaibhav gund");
+}
+
+
+//=======2========
+
+let sum =0;
+for(let i = 1;i<=10;i++){
+    sum = sum+i;
+
+}
+console.log("sum =",sum);
+
+//=====================
